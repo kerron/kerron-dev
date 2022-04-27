2e459f3a616701e5db9439929a1702d583118177
Kerron Parchment <kerron68@gmail.com>
2022-04-27 19:14:15 +0100

    feat: first deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
-import { Provider } from 'mobx-react'
-import { useStore } from '../store'
+import { Provider } from "mobx-react";
+import { useStore } from "../store";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDI_CACykWqDWe9l9CSIL1QG7fpa4SNVv0",
+  authDomain: "kerron-c5939.firebaseapp.com",
+  projectId: "kerron-c5939",
+  storageBucket: "kerron-c5939.appspot.com",
+  messagingSenderId: "574574568954",
+  appId: "1:574574568954:web:f21ddb276a500e50b57043",
+  measurementId: "G-F9B8YBNXVQ",
+};
+
+initializeApp(firebaseConfig);
 
 export default function App({ Component, pageProps }) {
-  const store = useStore(pageProps.initialState)
+  const store = useStore(pageProps.initialState);
 
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
-import SampleComponent from '../components/SampleComponent'
+import { getSnapshot } from "mobx-state-tree";
+import Home from "../components/Home";
+import { initializeStore } from "../store";
 
-export default function Home() {
-  return <SampleComponent title="Index Page" linkTo="/other" />
+export default function Ssr() {
+  return <Home />;
+}
+
+// The date returned here will be different for every request that hits the page,
+// that is because the page becomes a serverless function instead of being statically
+// exported when you use `getServerSideProps` or `getInitialProps`
+export function getServerSideProps() {
+  const store = initializeStore();
+
+  store.update();
+
+  return { props: { initialState: getSnapshot(store) } };
 }
