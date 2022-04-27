f2f0fceb2a0d81bbd11572107c4eaa18642fc0af
Kerron Parchment <kerron68@gmail.com>
2022-04-27 19:29:54 +0100

    feat: ssr rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,7 @@ export default function Ssr() {
   return <Home />;
 }
 
-// The date returned here will be different for every request that hits the page,
-// that is because the page becomes a serverless function instead of being statically
-// exported when you use `getServerSideProps` or `getInitialProps`
-export function getServerSideProps() {
+export function getStaticProps() {
   const store = initializeStore();
 
   store.update();
